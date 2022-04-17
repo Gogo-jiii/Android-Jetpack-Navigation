333fa77a82b031a15e6526e7d47d7516fc382b02
VgionTech <(0$t\U9i9b.{3k*>
2022-04-17 21:03:33 +0530

    init

---
+++ app/src/main/java/com/example/jetpacknavigation/FragmentThree.java
@@ -0,0 +1,38 @@
+package com.example.jetpacknavigation;
+
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentThree extends Fragment {
+
+    private View rootView;
+    private NavController navController;
+    private ToolbarManager toolbarManager;
+    private Toolbar toolbar;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_three, container, false);
+        navController = Navigation.findNavController(getActivity(), R.id.fragmentContainerView);
+
+        toolbarManager = ToolbarManager.getInstance();
+        toolbar = rootView.findViewById(R.id.toolbar3);
+
+        setupToolbar();
+
+        return rootView;
+    }
+
+    private void setupToolbar() {
+        toolbarManager.setupToolbar(getActivity(), navController, null, toolbar,
+                true);
+    }
+}
